efb4fa2c001a724d03e037bacee382ca1966131e
Mac <mac@Macs-MacBook-Pro.local>
2025-08-27 16:11:16 +0300

    add home page and connect it with products

--- task4-angular/src/app/app.ts
+++ task4-angular/src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';   
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink], 
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('task4-angular');
+  showHome = signal(true);
+
+  constructor(private router: Router) {}
+
+  goToProductsPage() {
+    this.showHome.set(false);
+    this.router.navigate(['/products']);
+  }
 }
