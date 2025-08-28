8eb7fb2ab5db7feff64e277624a80ad66589a1b2
Mac <mac@Macs-MacBook-Pro.local>
2025-08-28 11:46:10 +0300

    connect product service with products list

--- task4-angular/src/app/component/product-list-component/product-list-component.ts
+++ task4-angular/src/app/component/product-list-component/product-list-component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { GridModule } from '@progress/kendo-angular-grid';
 import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
 import { CategoryFilterPipe } from '../../pips/category-filter-pipe';
+import { ProductService } from '../../service/product-service/product-service';
 
 @Component({
   selector: 'app-product-list',
@@ -13,28 +14,22 @@ import { CategoryFilterPipe } from '../../pips/category-filter-pipe';
   styleUrls: ['./product-list-component.css']
 })
 export class ProductListComponent {
-  public products = [
-    { id: 1, name: 'Laptop', price: 1200, category: 'Electronics', addedDate: new Date(2025, 7, 20) },
-    { id: 2, name: 'Phone', price: 800, category: 'Electronics', addedDate: new Date(2025, 7, 21) },
-    { id: 3, name: 'Shoes', price: 150, category: 'Fashion', addedDate: new Date(2025, 7, 22) },
-    { id: 4, name: 'Watch', price: 300, category: 'Accessories', addedDate: new Date(2025, 7, 23) },
-    { id: 5, name: 'Bag', price: 100, category: 'Fashion', addedDate: new Date(2025, 7, 24) },
-    { id: 6, name: 'Headphones', price: 200, category: 'Electronics', addedDate: new Date(2025, 7, 25) },
-    { id: 7, name: 'Jacket', price: 250, category: 'Fashion', addedDate: new Date(2025, 7, 26) },
-    { id: 8, name: 'Sunglasses', price: 180, category: 'Accessories', addedDate: new Date(2025, 7, 27) },
-    { id: 9, name: 'Tablet', price: 600, category: 'Electronics', addedDate: new Date(2025, 7, 28) },
-    { id: 10, name: 'Camera', price: 900, category: 'Electronics', addedDate: new Date(2025, 7, 29) },
-    { id: 11, name: 'Belt', price: 80, category: 'Accessories', addedDate: new Date(2025, 7, 30) }
-  ];
-
-  public categories = [...new Set(this.products.map(p => p.category))];
-
   public selectedCategory: string | null = null;
+
+  products;
+  categories;
+
+  constructor(private productService: ProductService) {
+    this.products = this.productService.products;
+
+    this.categories = computed(() =>
+      [...new Set(this.products().map(p => p.category))]
+    );
+  }
 
   public whenChangeCategory(value: any): void {
     if (typeof value === 'string') {
       this.selectedCategory = value === '-- All --' ? null : value;
     }
   }
-
 }
