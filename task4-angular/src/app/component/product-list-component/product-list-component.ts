4c76177d23f77e8d34e6e10f75eeee9363331d02
Mac <mac@Macs-MacBook-Pro.local>
2025-08-28 11:12:19 +0300

    put the components in the same file and make the same for services

--- task4-angular/src/app/component/product-list-component/product-list-component.ts
+++ task4-angular/src/app/component/product-list-component/product-list-component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { GridModule } from '@progress/kendo-angular-grid';
 import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
-import { CategoryFilterPipe } from '../pips/category-filter-pipe';
+import { CategoryFilterPipe } from '../../pips/category-filter-pipe';
 
 @Component({
   selector: 'app-product-list',
