d11a9848d3413eb7e5e5abb90bce99df3476a9ab
Mac <mac@Macs-MacBook-Pro.local>
2025-08-28 10:57:51 +0300

    add product detail commponent

---
+++ task4-angular/src/app/product-detail-component/product-detail-component.ts
@@ -0,0 +1,13 @@
+import { Component, computed, effect } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-detail-component.html',
+  styleUrls: ['./product-detail-component.css']
+})
+export class ProductDetailComponent {
+
+}
