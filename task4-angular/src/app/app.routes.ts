2b15472d96494d509a5084de77a6e092e8b59a91
Mac <mac@Macs-MacBook-Pro.local>
2025-08-28 15:11:30 +0300

    add product details html and css

--- task4-angular/src/app/app.routes.ts
+++ task4-angular/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './component/product-list-component/product-list-component';
+import { ProductDetailComponent } from './component/product-detail-component/product-detail-component';
 
 export const routes: Routes = [
   { path: 'products', component: ProductListComponent },
+  { path: 'product/:id', component: ProductDetailComponent },
   { path: '**', redirectTo: '' }
 ];
--- task4-angular/src/app/component/product-detail-component/product-detail-component.ts
+++ task4-angular/src/app/component/product-detail-component/product-detail-component.ts
@@ -1,13 +1,19 @@
-import { Component, computed, effect } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductService } from '../../service/product-service/product-service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './product-detail-component.html',
   styleUrls: ['./product-detail-component.css']
 })
 export class ProductDetailComponent {
+  constructor(public productService: ProductService) {}
 
+  backToPreviousPage() {
+    history.back();
+  }
 }
